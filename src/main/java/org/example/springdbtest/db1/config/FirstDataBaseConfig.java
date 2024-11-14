a3958d0f67e78e3f574eb8d80dd1e8839b417edf
kostikkv84 <kostikkv84@mail.ru>
2024-11-14 11:05:15 +0300

    commit 14-11-2024

--- src/main/java/org/example/springdbtest/db1/config/FirstDataBaseConfig.java
+++ src/main/java/org/example/springdbtest/db1/config/FirstDataBaseConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.sql.DataSource;
 
 @Configuration
+@EnableAspectJAutoProxy
 @EnableJpaRepositories(
         basePackages = "org.example.springdbtest.db1.repository", // Пакет репозиториев для первой БД
         entityManagerFactoryRef = "firstEntityManagerFactory",
